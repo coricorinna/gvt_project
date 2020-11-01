513e2434e367f332fc93d1abf28a6196f21c395b
coricorinna <corinna.sch24@gmail.com>
2020-11-01 21:51:16 +0100

    esa2

--- esa2/esa2.js
+++ esa2/esa2.js
@@ -11,7 +11,7 @@ let vs = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vs, vsSource);
 gl.compileShader(vs);
 
-fsSouce =  'void main() { gl_FragColor = vec4(0, 0, 0, 1); }'; //Farbe der Linie
+fsSouce =  'void main() { gl_FragColor = vec4(50, 0.51, 0.80, 1); }'; //Farbe der Linie
 let fs = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fs, fsSouce);
 gl.compileShader(fs);
@@ -24,26 +24,40 @@ gl.useProgram(prog);
 
 var vertices = new Float32Array([ 
 
+    7,5,
+    7,1,
+    0,-1,
+    0,-0.5,
+
+    7, 1.5,
+    7,-1,
+    0,-1,
     0,0,
-    0,5,
-    5,5,
-    5,0,
+    
+    0,0,
+    0,3,
+    3,3,
+    3,0,
+    
+    0.5,0.5,
+    0.5,4,
+    4,4,
+    4,0.5,
 
     1,1,
-    1,6,
+    1,5,
+    5,5,
+    5,1,
+
+    1.5,1.5,
+    1.5,6,
     6,6,
-    6,1,
+    6,1.5,
 
     2,2,
     2,7,
     7,7,
     7,2,
-
-    3,3,
-    3,8,
-    8,8,
-    8,3,
-
 ]);
 
 let vbo = gl.createBuffer();
@@ -56,4 +70,4 @@ gl.vertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 0, 0);
 gl.enableVertexAttribArray(posAttrib);
 
 gl.lineWidth(5.0);
-gl.drawArrays(gl.LINE_STRIP, 0, 16);
+gl.drawArrays(gl.LINE_STRIP, 0, 30);
